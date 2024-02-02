ba83c8eef9376215d96baa4c7d6aa04a35585e9f
af723 <andy.fu23@imperial.ac.uk>
2024-02-02 00:23:57 +0000

    Added dummy classes

---
+++ database.py
@@ -0,0 +1,15 @@
+class Database:
+    def __init__(self):
+        pass
+
+    def get(self, key):
+        pass
+
+    def set(self, key, value):
+        pass
+
+    def delete(self, key):
+        pass
+
+    def register(self, key):
+        pass
---
+++ preprocessor.py
@@ -0,0 +1,15 @@
+class Preprocessor():
+    def __init__(self, data, Database):
+        self.data = data
+        self.Database = Database
+
+    def preprocess(self):
+        ## todo: preprocess data
+        return self.data
+    
+    def save(self, mrn):
+        self.Database.save(self.data)
+
+    def load(self, mrn):
+        self.data = self.Database.load(mrn)
+        return self.data

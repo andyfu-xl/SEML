7fc3af148d4b45ac8fc646d560f1384f08bcaafb
af723 <andy.fu23@imperial.ac.uk>
2024-02-03 23:27:34 +0000

    database updated

--- modules/database.py
+++ modules/database.py
@@ -9,15 +9,13 @@ class Database:
     def load_csv(self, file_path):
         with open('history.csv', newline='') as csvfile:
             reader = csv.reader(csvfile)
-            # initialize empyt dictionary, the key is patient's mrn and the value is patient's history
             self.data = {}
-            # Iterate over each row in the CSV file
             for i, row in enumerate(reader):
                 if i == 0:
                     continue
                 mrn = row[0]
                 if mrn not in self.data:
-                    self.data[mrn] = row[1:]
+                    self.data[mrn] = patientinfo = {"test_results": row[1:], "gender" : None, "dob" : None}
                 else:
                     raise Exception('Duplicate MRN found:', mrn)
 
@@ -25,11 +23,26 @@ class Database:
         if mrn in self.data:
             return self.data[mrn]
 
-    def set(self, key, value):
-        pass
+    def set(self, mrn, date, value):
+        if mrn in self.data:
+            self.data[mrn]["test_results"].append((date, value))
+        else:
+            raise Exception('Error: Trying to set test results for a non-existing patient, MRN not found:', mrn)
+        print("Test result added successfully")
+            
 
-    def delete(self, key):
-        pass
+    def delete(self, mrn):
+        if mrn in self.data:
+            del self.data[mrn]
+        else:
+            raise Exception('Error: Trying to discharging test results for a non-existing patient, MRN not found:', mrn)
+        print("Patient discharged successfully")
+        
 
-    def register(self, key):
-        pass
+    def register(self, mrn, gender, dob):
+        if mrn not in self.data:
+            self.data[mrn] = {"test_results": [], "gender": gender, "dob": dob}
+        else:
+            self.data[mrn]["gender"] = gender
+            self.data[mrn]["dob"] = dob
+        print("Patient registered successfully")

06fe5b532b73ec182b9ee28fef80b6a9b252f2b9
Esmanda Wong <esmanda3w@gmail.com>
2024-02-05 19:46:32 +0000

    implement communicator and skeleton for main

--- main.py
+++ main.py
@@ -1,5 +1,43 @@
+import time
+
+from modules.communicator.communicator import Communicator
+# from modules.dataparser.dataparser import DataParser
+# from modules.preprocessor import Preprocessor
+# from modules.model import Model
+
 def main():
-    pass
+    communicator = Communicator("localhost", 8440)
+    # dataparser = DataParser()
+    # preprocessor = Preprocessor()
+    # model = Model()
+    
+    while True:
+        # Receive message
+        message = communicator.receive()
+        if message == None:
+            break
+        print(message)
+
+        # Pass the message to data parser
+        # TODO
+        # parsed_message = dataparser.parse(message)
+
+        # Process message
+        # TODO
+        # preprocessed_message = preprocessor.preprocess(parsed_message)
+
+        # Perform inference
+        # TODO
+        # has_aki, mrn = model.predict(preprocessed_message)
+
+        # Page (if necessary)
+        # TODO
+        # if has_aki:
+        #     communicator.page(mrn)
+
+        # Acknowledge message
+        communicator.acknowledge()
+        time.sleep(1)
 
 if __name__ == "__main__":
     main()

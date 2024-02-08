92383d03c521f82f9a50165d28d19942331d3b7d
Seriously <seriously@SeriouslydeMacBook-Pro.local>
2024-02-08 17:14:14 +0000

    integration_test fixed

--- main.py
+++ main.py
@@ -11,33 +11,28 @@ from modules.preprocessor import Preprocessor
 from modules.model import load_model, inference, save_inference_results
 
 def main():
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     communicator = Communicator("localhost", 8440, 8441)
     dataparser = DataParser()
     database = Database()
     database.load_csv('./data/history.csv')
     preprocessor = Preprocessor(database)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model = load_model('./lstm_model_new.pth').to(device)
-    mrn_aki = []
-    date_aki = []
+    model = load_model('./lstm_model.pth').to(device)
+
     while True:
         # Receive message
         message = communicator.receive()
         if message == None:
-            save_inference_results(mrn_aki, date_aki, "mrn_aki.csv")
             break
 
         # Pass the message to data parser
         parsed_message = dataparser.parse_message(message)
         
         mrn = parsed_message.mrn
-        if parsed_message.message_type == 'ORU^R01':
-            date = parsed_message.obr_timestamp
 
         # Process message
         preprocessed_message = preprocessor.preprocess(parsed_message)
-        # if mrn == "701783" and parsed_message.message_type == 'ORU^R01':
-        #     print(preprocessed_message, date)
 
         # Perform inference
         has_aki = False
@@ -48,8 +43,6 @@ def main():
         if has_aki:
             print(f"ALERT: Patient {mrn} has AKI")
             communicator.page(mrn)
-            mrn_aki.append(mrn)
-            date_aki.append(date)
             database.paged(mrn)
 
         # Acknowledge message
